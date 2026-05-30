f96c8b511082d4ad8000e894ef3b9d57658d10d5
ARUN PAL <arunpal6390@gmail.com>
2026-05-30 12:39:36 +0530

    new code added

---
+++ Concept/notes.js
@@ -0,0 +1,55 @@
+// let a = 88;
+// let isOnline = {a:8, ab: 88};
+// let age = 88;
+// console.log(typeof(isOnline));
+// console.log(a);
+// console.log(age);
+
+// //primitive store one value at a time.
+
+// function
+// A function is a resuable block of code that perform a specific task.
+// set of statement that executes only wehen you called.
+// Built-in, - javascript already provides
+// user define -  developers make these function.
+/*
+ function demo(){
+      alert("Hii");
+    }
+
+
+*/
+// function tryp() {
+//   let a = 88;
+//   let b = 44;
+//   alert(a + b);
+// }
+
+// operators
+// let number1 = 88; // Assignment operators
+// let number2 = 77;
+
+// console.log("Addition of two nubers:", number1 + number2); // Arithematic operator
+// console.log(number1 == number2); // comparison operators
+// if ((number1 && number2) >80) {
+//   console.log(number1 + number2);
+// } else {
+//   console.log("Given nuber is too much greator")
+// }
+
+// let name = 'Null';
+// let age = 88;
+// // if (age == 88 && name == "abc") {
+// //   console.log("yes");
+// // } else {
+// //   console.log("no");
+// // }
+// for (let num = 1; num <= 3; num++){ //outer loop
+  
+//   for (let j = 1; j <= 5; j++){ // ineer loop
+//     console.log("j: ", j);
+//   }
+
+//   console.log("****");
+// }
+
--- Questions/question1.js
+++ Questions/question1.js
@@ -1,1 +1,14 @@
-console.log("Hii bro how are you, I am fine,tell about yourself")
+// num = 19;
+// ans = num > 18 ? "adult" : "minor";//ternary operator
+// console.log(ans);
+// for of - used in string ,array
+// for in - used in object.
+name = "biology";
+let count = 0;
+for (let char of name) {
+  count++;
+  console.log(`name: ${char}`);
+}
+console.log(`length of string is: ${count}`);
+
+
